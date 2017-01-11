008924065bf9df0d3fd13383a49864dc2c358972
Justin Merz <jrmerz@gmail.com>
2017-01-11 14:24:32 -0800

    registering events on load and attach

--- dist/build.js
+++ dist/build.js
@@ -8,21 +8,39 @@ var EventBusBehavior = function () {
     return window.EventBus;
   }
 
-  return {
-    attached: function attached() {
-      this._eventBus = getEventBus(this);
+  function init() {
+    if (this._eb_handlersSet) return;
 
-      if (!this._eventBus || !this.ebBind) return;
-      for (var key in this.ebBind) {
-        if (!this[this.ebBind[key]]) {
-          console.warn(this.nodeName + " could not bind event " + key + " to " + this.ebBind[key]);
-          continue;
-        }
-        this._eventBus.on(key, this[this.ebBind[key]].bind(this));
+    this._eventBus = getEventBus(this);
+    this._eb_handlersSet = true;
+
+    if (!this._eventBus || !this.ebBind) return;
+    for (var key in this.ebBind) {
+      if (!this[this.ebBind[key]]) {
+        console.warn(this.nodeName + " could not bind event " + key + " to " + this.ebBind[key]);
+        continue;
       }
-    },
+      this._eventBus.on(key, this[this.ebBind[key]].bind(this));
+    }
+  }
+
+  return {
+    // have we initialized the handlers?
+    _eb_handlersSet: false,
+    // do we want to detach listeners when element is detached
+    // (prevent memory leaks)
+    _eb_unregisterOnDetach: true,
+
+    ready: init,
+
+    attached: init,
+
     detached: function detached() {
+      if (!this._eb_unregisterOnDetach) return;
+
+      this._eb_handlersSet = false;
       if (!this._eventBus || !this.ebBind) return;
+
       for (var key in this.ebBind) {
         if (!this[this.ebBind[key]]) continue;
         this._eventBus.removeListener(key, this[this.ebBind[key]]);
