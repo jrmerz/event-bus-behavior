d6e1d3b0335aca78637600aa07ea513de2126a18
Justin Merz <jrmerz@gmail.com>
2017-03-21 16:52:17 -0700

    bug fix for detaching listener

--- dist/build.js
+++ dist/build.js
@@ -1,31 +1,25 @@
 'use strict';
 
 var EventBusBehavior = function () {
-  function _getEventBus(ele) {
-    if (ele.EVENT_BUS && window[ele.EVENT_BUS]) {
-      return window[ele.EVENT_BUS];
-    }
-    return window.EventBus;
-  }
-
   function init() {
     if (this._eb_handlersSet) return;
 
-    this._eventBus = _getEventBus(this);
     this._eb_handlersSet = true;
 
-    if (!this._eventBus || !this.ebBind) return;
+    if (!this.getEventBus() || !this.ebBind) return;
     for (var key in this.ebBind) {
       if (!this[this.ebBind[key]]) {
         console.warn(this.nodeName + ' could not bind event ' + key + ' to ' + this.ebBind[key]);
         continue;
       }
-      this._eventBus.on(key, this[this.ebBind[key]].bind(this));
+
+      this[this.ebBind[key]] = this[this.ebBind[key]].bind(this);
+      this.getEventBus().on(key, this[this.ebBind[key]]);
     }
   }
 
   function ebChain(events, done) {
-    triggerEbChainEvent(0, events, [], this._eventBus, done);
+    triggerEbChainEvent(0, events, [], this.getEventBus(), done);
   }
 
   function triggerEbChainEvent(index, events, results, eventBus, done) {
@@ -72,19 +66,19 @@ var EventBusBehavior = function () {
       if (!this._eb_unregisterOnDetach) return;
 
       this._eb_handlersSet = false;
-      if (!this._eventBus || !this.ebBind) return;
+      if (!this.getEventBus() || !this.ebBind) return;
 
       for (var key in this.ebBind) {
         if (!this[this.ebBind[key]]) continue;
-        this._eventBus.removeListener(key, this[this.ebBind[key]]);
+        this.getEventBus().removeListener(key, this[this.ebBind[key]]);
       }
     },
 
     getEventBus: function getEventBus() {
-      if (!this._eventBus) {
-        this._eventBus = _getEventBus(this);
+      if (this.EVENT_BUS && window[this.EVENT_BUS]) {
+        return window[this.EVENT_BUS];
       }
-      return this._eventBus;
+      return window.EventBus;
     },
 
     ebEmit: function ebEmit(event, payload) {
